942c12b3df987f51ced6b04e086f660859aef46f
Devapriyanpr <107738211+Devapriyanpr@users.noreply.github.com>
2022-07-05 08:17:16 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Pattern2
+   {
+    public static void main(String[]args)
+      {
+      Scanner scn=new Scanner(System.in);
+      System.out.println("Enter the number of Rows=");
+      int Rows=scn.nextInt();
+     for(int i=1;i<=Rows;i++)
+        {                
+            for(int j=1;j<=i;j++)
+            {              
+            if(i==1||i==Rows||j==1||j==Rows)
+                { 
+                System.out.print("* ");
+                }
+            else
+                {
+                 System.out.print(" ");
+                } 
+                  
+            } 
+                System.out.println();
+       }
+       
+      }
+   }
